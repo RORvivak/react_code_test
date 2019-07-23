ab5f18b05cad1db6a2c1e9a970cd0ae053596c4e
vivak <vivak.k@philips.com>
2019-07-23 16:14:37 +0530

    removal of warning message

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,14 +10,11 @@ const reducer = (state = initialstate, action) => {
                 let payload = action.payload
                 let id = state.results.length + 1
                 state.results.push({title: payload.title, description: payload.description, status: "created", id: id})
-               
-                console.log(state)
                 return({...state})
             }
 
             case "assing_task":{
                 let assign_state = [...state.results]
-                // console.log(assign_state)
                 assign_state.map((e)=>{
                     if(e.id == action.payload.id)
                     {
--- src/task_listing.js
+++ src/task_listing.js
@@ -3,25 +3,27 @@ import { connect } from 'react-redux';
 
 class  TaskListing extends Component {
     render(){
-        const results = this.state
-        console.log(results)
     return(
         <table border = "1">
-            <tr>
-                <td>Title</td>
-                <td>Description</td>
-                <td>Assign</td>
-             </tr>   
-       { this.props.ctr.map((e,i)=>{
-           let button = <button onClick =  { () => this.props.assign(e.id)}>Assign</button>
-           if(e.status != "created")
-            {button  = "Already assigned" }
-            return(<tr>
-                <td>{e.title}</td>
-                <td>{e.description}</td>
-                <td>{button}</td>
-            </tr>)
-        })}
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Assign</th>
+                </tr>    
+            </thead>
+            <tbody>   
+                { this.props.ctr.map((e,i)=>{
+                    let button = <button onClick =  { () => this.props.assign(e.id)}>Assign</button>
+                    if(e.status != "created")
+                        {button  = "Already assigned" }
+                        return(<tr key={e.id}>
+                            <td>{e.title}</td>
+                            <td>{e.description}</td>
+                            <td>{button}</td>
+                        </tr>)
+                    })}
+            </tbody>
         </table>
       
     )
